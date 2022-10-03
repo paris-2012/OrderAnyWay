0dc5901ba35e111795ce406d337457f0145adca3
paris2012 <roadtolowpriority@gmail.com>
2022-10-03 09:33:59 -0500

    corrected order tracking to have route

--- app/src/main/java/com/example/fooddeliveryapp/view/TrackingActivity.kt
+++ app/src/main/java/com/example/fooddeliveryapp/view/TrackingActivity.kt
@@ -1,62 +1,160 @@
 package com.example.fooddeliveryapp.view
 
 import android.annotation.SuppressLint
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.drawable.Drawable
+import android.os.AsyncTask
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fooddeliveryapp.R
 import com.example.fooddeliveryapp.databinding.ActivityTrackingBinding
+import com.example.fooddeliveryapp.model.remote.MapData
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
+import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 
 class TrackingActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
-    private lateinit var binding: ActivityTrackingBinding
+    private var originLatitude: Double = 35.91152724145574
+    private var originLongitude: Double = -79.06088393314307
+    private var deliveryLatitude: Double = 35.938755566390725
+    private var deliveryLongitude: Double = -79.02322571882706
+    private var destinationLatitude: Double = 35.94264364313495
+    private var destinationLongitude: Double = -79.0039404174305
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityTrackingBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
+        setContentView(R.layout.activity_tracking)
+        // Fetching API_KEY which we wrapped
+        val ai: ApplicationInfo = applicationContext.packageManager
+            .getApplicationInfo(applicationContext.packageName, PackageManager.GET_META_DATA)
+        val value = ai.metaData["com.google.android.geo.API_KEY"]
+        val apiKey = value.toString()
+        // Initializing the Places API with the help of our API_KEY
+        // Map Fragment
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-    }
-
-    @SuppressLint("UseCompatLoadingForDrawables")
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
         val deliveryIcon =
             getMarkerIconFromDrawable(resources.getDrawable(R.drawable.ic_baseline_directions_car_50_bright_orange))
         val homeIcon =
             getMarkerIconFromDrawable(resources.getDrawable(R.drawable.ic_baseline_home_50_bright_orange))
         val storeIcon =
             getMarkerIconFromDrawable(resources.getDrawable(R.drawable.ic_baseline_storefront_50_bright_orange))
-        val homeLocation = LatLng(35.141060, -79.003170)
-        val deliveryLocation = LatLng(35.141060 + .001, -79.003170 + .001)
-        val storeLocation = LatLng(35.141060 + .002, -79.003170 + .002)
-        mMap.addMarker(
-            MarkerOptions()
-                .position(deliveryLocation)
-                .icon(deliveryIcon)
-        )
-        mMap.addMarker(
-            MarkerOptions()
-                .position(homeLocation)
-                .icon(homeIcon)
-        )
-        mMap.addMarker(
-            MarkerOptions()
-                .position(storeLocation)
-                .icon(storeIcon)
-        )
+            mapFragment.getMapAsync {
+                mMap = it
+                val originLocation = LatLng(originLatitude, originLongitude)
+                mMap.addMarker(MarkerOptions()
+                    .position(originLocation)
+                    .icon(storeIcon)
+                )
+                val deliveryLocation = LatLng(deliveryLatitude, deliveryLongitude)
+                mMap.addMarker(MarkerOptions()
+                    .position(deliveryLocation)
+                    .icon(deliveryIcon)
+                )
+                val destinationLocation = LatLng(destinationLatitude, destinationLongitude)
+                mMap.addMarker(MarkerOptions()
+                    .position(destinationLocation)
+                    .icon(homeIcon)
+                )
+                var urll = getDirectionURL(originLocation, deliveryLocation, apiKey)
+                GetDirection(urll).execute()
+                urll = getDirectionURL(deliveryLocation, destinationLocation, apiKey)
+                GetDirection(urll).execute()
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(originLocation, 13F))
+            }
+    }
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(deliveryLocation, 16f))
+    override fun onMapReady(p0: GoogleMap) {
+        mMap = p0
+        val originLocation = LatLng(originLatitude, originLongitude)
+        mMap.clear()
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(originLocation, 18F))
+    }
+
+    private fun getDirectionURL(origin: LatLng, dest: LatLng, secret: String): String {
+        return "https://maps.googleapis.com/maps/api/directions/json?origin=${origin.latitude},${origin.longitude}" +
+                "&destination=${dest.latitude},${dest.longitude}" +
+                "&sensor=false" +
+                "&mode=driving" +
+                "&key=$secret"
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private inner class GetDirection(val url: String) :
+        AsyncTask<Void, Void, List<List<LatLng>>>() {
+        override fun doInBackground(vararg params: Void?): List<List<LatLng>> {
+            val client = OkHttpClient()
+            val request = Request.Builder().url(url).build()
+            val response = client.newCall(request).execute()
+            val data = response.body!!.string()
+
+            val result = ArrayList<List<LatLng>>()
+            try {
+                val respObj = Gson().fromJson(data, MapData::class.java)
+                val path = ArrayList<LatLng>()
+                for (i in 0 until respObj.routes[0].legs[0].steps.size) {
+                    path.addAll(decodePolyline(respObj.routes[0].legs[0].steps[i].polyline.points))
+                }
+                result.add(path)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            return result
+        }
+
+        override fun onPostExecute(result: List<List<LatLng>>) {
+            val lineoption = PolylineOptions()
+            for (i in result.indices) {
+                lineoption.addAll(result[i])
+                lineoption.width(10f)
+                lineoption.color(Color.parseColor("#D83704"))
+                lineoption.geodesic(true)
+            }
+            mMap.addPolyline(lineoption)
+        }
+    }
+
+    fun decodePolyline(encoded: String): List<LatLng> {
+        val poly = ArrayList<LatLng>()
+        var index = 0
+        val len = encoded.length
+        var lat = 0
+        var lng = 0
+        while (index < len) {
+            var b: Int
+            var shift = 0
+            var result = 0
+            do {
+                b = encoded[index++].code - 63
+                result = result or (b and 0x1f shl shift)
+                shift += 5
+            } while (b >= 0x20)
+            val dlat = if (result and 1 != 0) (result shr 1).inv() else result shr 1
+            lat += dlat
+            shift = 0
+            result = 0
+            do {
+                b = encoded[index++].code - 63
+                result = result or (b and 0x1f shl shift)
+                shift += 5
+            } while (b >= 0x20)
+            val dlng = if (result and 1 != 0) (result shr 1).inv() else result shr 1
+            lng += dlng
+            val latLng = LatLng((lat.toDouble() / 1E5), (lng.toDouble() / 1E5))
+            poly.add(latLng)
+        }
+        return poly
     }
     private fun getMarkerIconFromDrawable(drawable: Drawable): BitmapDescriptor? {
         val canvas = Canvas()
